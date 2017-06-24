45f570de6a7ee8a49d20e10ca7b8bf9ef54a0f75
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-06-24 15:19:37 +0530

    updating

--- validators.js
+++ validators.js
@@ -2,7 +2,7 @@ var validators = require('validators');
 var Clients = require('model-clients');
 
 exports.create = function (req, res, next) {
-    validators.pre({
+    validators.create({
         model: Clients
     }, req, res, next);
 };
